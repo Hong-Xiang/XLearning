c855bf970f8f70f0c9fe48742024f58901488da7
HongXiang <hx.hongxiang@gmail.com>
2017-04-26 11:20:43 +0800

    add switch to turn off clip.

---
+++ fast_impl/cali_valid.py
@@ -0,0 +1,8 @@
+from keras.layers import Conv2D, Dense, Flatten, ELU, Input, concatenate, Dropout, MaxPool2D, add
+from keras.models import Model
+from keras.optimizers import Adam, RMSprop
+from keras.utils import to_categorical
+import numpy as np
+import sys
+import random
+
