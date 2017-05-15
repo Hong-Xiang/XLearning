35e7a0f7b853ababa274a305a3c3ded8a5d47bf8
HongXiang <hx.hongxiang@gmail.com>
2017-05-15 23:28:16 +0800

    crop layer

--- nets/superresolution.py
+++ nets/superresolution.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from ..utils.general import with_config
 from .base import Net
-from ..models.image import conv2d, upsampling2d
+from ..models.image import conv2d, upsampling2d, align_by_crop
 from tensorflow.contrib import slim
 import numpy as np
 from ..utils.general import analysis_device
@@ -108,8 +108,7 @@ class SRNet1(SRNetBase):
         with tf.name_scope(name):
             h = low_res            
             interp = upsampling2d(h, size=self.params['down_sample_ratio'])                        
-            res_ref = high_res - interp       
-            err_itp = tf.abs(res_ref)
+ 
             
             h = tf.layers.conv2d(interp, 64, 5, padding='same',
                                 name='conv_stem', activation=tf.nn.elu, reuse=reuse)
@@ -125,11 +124,16 @@ class SRNet1(SRNetBase):
                 
                 if with_summary:
                     tf.summary.histogram('activ_%d' % i, h)
-            res_inf = tf.layers.conv2d(h, 1, 5, padding='same', name='conv_end', reuse=reuse)            
+            res_inf = tf.layers.conv2d(h, 1, 5, padding='same', name='conv_end', reuse=reuse)                      
+                             
             sr_inf = interp + res_inf
+                        
+           
+
+            res_ref = high_res - interp  
+            err_itp = tf.abs(res_ref)  
             err_inf = tf.abs(high_res - sr_inf)
-            
-            patch_size = self.p.high_shape[1] * self.p.high_shape[2]
+            # patch_size = self.p.high_shape[1] * self.p.high_shape[2]
             loss = tf.losses.mean_squared_error(high_res, sr_inf)
             grad = self.optimizers['train'].compute_gradients(loss)
             
@@ -175,6 +179,7 @@ class SRNet1(SRNetBase):
         sr_infs = []
         losses = []
         grads = []
+
         for i in range(self.p.nb_gpus):
             device = '/gpu:%d'%i
             with tf.device(device):
@@ -189,7 +194,7 @@ class SRNet1(SRNetBase):
         with tf.name_scope('infer'):
             sr_inf = tf.concat(sr_infs, axis=0)
             self.add_node(sr_inf, 'inference')
-        
+        tf.summary.scalar('lr/train', self.lr['train'])
         with tf.name_scope('cpu_summary'):
             interp = upsampling2d(low_res, size=self.params['down_sample_ratio'])                        
             res_ref = high_res - interp
@@ -381,6 +386,164 @@ class SRNet2(SRNetBase):
         self.feed_dict['train'] = ['data', 'label']
         self.feed_dict['predict'] = ['data']
         self.feed_dict['summary'] = ['data', 'label']
+
+        self.run_op['train'] = {'train_step': self.train_steps['train'],
+                                'loss': self.losses['train'], 'global_step': self.gs}
+        self.run_op['predict'] = {'infernce': sr_inf}
+        self.run_op['summary'] = {'summary_all': self.summary_ops['all']}
+    
+    def _set_train(self):
+        pass
+
+
+class SRNet3(SRNetBase):
+    @with_config
+    def __init__(self,
+                 filters=64,
+                 depths=20,
+                 train_verbose=0,
+                 is_bn=True,
+                 is_res=True,
+                 **kwargs):
+        SRNetBase.__init__(self, **kwargs)
+        self.params['name'] = "SRNet3"
+        self.params['filters'] = filters
+        self.params['depths'] = depths
+        self.params['train_verbose'] = train_verbose
+        self.params['is_bn'] = is_bn
+        self.params['is_res'] = is_res
+        self.params.update_short_cut()
+    
+    def super_resolution(self, low_res, high_res, with_summary=False, reuse=None, name=None):
+        with tf.name_scope(name):
+            h = low_res            
+            interp = upsampling2d(h, size=self.params['down_sample_ratio'])                        
+
+            
+            h = tf.layers.conv2d(interp, 64, 5, padding='same',
+                                name='conv_stem', activation=tf.nn.elu, reuse=reuse)
+            for i in range(self.params['depths']):
+                hpre = h
+                h = tf.layers.conv2d(
+                    h, self.params['filters'], 3, padding='same', name='conv_%d' % i, reuse=reuse)
+                if self.p.is_bn:
+                    h = tf.layers.batch_normalization(h, training=self.training, reuse=reuse, name='bn_%d'%i, scale=False)
+                if self.p.is_res:
+                    h = 0.2 * h + hpre
+                h = tf.nn.relu(h)
+                
+                if with_summary:
+                    tf.summary.histogram('activ_%d' % i, h)
+            res_inf = tf.layers.conv2d(h, 1, 5, padding='same', name='conv_end', reuse=reuse)            
+            sr_inf = interp + res_inf
+            
+            res_inf, sr_inf, interp = align_by_crop(high_res, [res_inf, sr_inf, interp])
+
+            err_inf = tf.abs(high_res - sr_inf)
+            res_ref = high_res - interp
+            err_itp = tf.abs(res_ref)
+
+            patch_size = self.p.high_shape[1] * self.p.high_shape[2]
+            loss = tf.losses.mean_squared_error(high_res, sr_inf)
+            grad = self.optimizers['train'].compute_gradients(loss)
+            
+            if with_summary:
+                tf.summary.image('low_res', low_res)
+                tf.summary.image('high_res', high_res)
+                tf.summary.image('interp', interp)
+                tf.summary.image('sr_inf', sr_inf)
+                tf.summary.image('res_ref', res_ref)
+                tf.summary.image('res_inf', res_inf)
+                tf.summary.image('err_itp', err_itp)
+                tf.summary.image('err_inf', err_inf)
+                tf.summary.scalar('loss', loss)            
+            return sr_inf, loss, grad, interp, high_res
+                
+
+    def _set_model(self):
+        bs_gpu = self.p.batch_size // self.p.nb_gpus
+        sliced_low_res = []
+        sliced_high_res = []
+        with tf.device('/cpu:0'):
+            with tf.name_scope('low_resolution'):
+                low_res = tf.placeholder(
+                    dtype=tf.float32, shape=self.params['low_shape'], name='low_resolution')
+                self.add_node(low_res, 'data')
+                gpu_shape = low_res.shape.as_list()
+                gpu_shape[0] = bs_gpu
+                for i in range(self.p.nb_gpus):
+                    with tf.name_scope('device_%d'%i):
+                        sliced_low_res.append(tf.slice(low_res, [i*bs_gpu, 0, 0, 0], gpu_shape))                
+            with tf.name_scope('high_resolution'):
+                high_res = tf.placeholder(                
+                    dtype=tf.float32, shape=self.params['high_shape'], name='high_resolution')
+                self.add_node(high_res, 'label')
+                gpu_shape = high_res.shape.as_list()
+                gpu_shape[0] = bs_gpu
+                gpu_shape[1] -= 2*self.p.crop_size
+                gpu_shape[2] -= 2*self.p.crop_size
+                for i in range(self.p.nb_gpus):
+                    with tf.name_scope('device_%d'%i):
+                        sliced_high_res.append(tf.slice(high_res, [i*bs_gpu, 0, 0, 0], gpu_shape))                
+            self.optimizers['train'] = tf.train.AdamOptimizer(self.lr['train'])
+            self.super_resolution(sliced_low_res[0], sliced_high_res[0], with_summary=False, reuse=None, name='cpu_tower')
+        
+        sr_infs = []
+        losses = []
+        grads = []
+        interps = []
+        high_ress = []
+        for i in range(self.p.nb_gpus):
+            device = '/gpu:%d'%i
+            with tf.device(device):
+                sr_inf, loss, grad, interp, high_res = self.super_resolution(sliced_low_res[i], sliced_high_res[i], with_summary=False, reuse=True, name='gpu_%d'%i)
+            sr_infs.append(sr_inf)
+            losses.append(loss)
+            grads.append(grad)
+            interps.append(interp)
+            high_ress.append(high_res)
+
+        with tf.name_scope('loss'):
+            self.losses['train'] = tf.add_n(losses)
+        
+        with tf.name_scope('infer'):
+            sr_inf = tf.concat(sr_infs, axis=0)
+            self.add_node(sr_inf, 'inference')
+            interp = tf.concat(interps, axis=0)
+            high_res = tf.concat(high_ress, axis=0)
+        tf.summary.scalar('lr/train', self.lr['train'])
+        with tf.name_scope('cpu_summary'):
+            
+            res_ref = high_res - interp
+            res_inf = high_res - sr_inf      
+            err_itp = tf.abs(res_ref)            
+            err_inf = tf.abs(res_inf)
+            l2_err_itp = tf.reduce_sum(tf.square(err_itp), axis=[1, 2, 3])
+            l2_err_inf = tf.reduce_sum(tf.square(err_inf), axis=[1, 2, 3])
+
+            l2_inf = tf.reduce_mean(l2_err_inf)
+            l2_itp = tf.reduce_mean(l2_err_itp)
+            ratio = tf.reduce_mean(l2_err_inf/(l2_err_itp+1e-3))
+            tf.summary.image('low_res', low_res)
+            tf.summary.image('high_res', high_res)
+            tf.summary.image('interp', interp)
+            tf.summary.image('sr_inf', sr_inf)
+            tf.summary.image('res_ref', res_ref)
+            tf.summary.image('res_inf', res_inf)
+            tf.summary.image('err_itp', err_itp)
+            tf.summary.image('err_inf', err_inf)
+            tf.summary.scalar('loss', self.losses['train'])     
+            tf.summary.scalar('ratio', ratio)
+            tf.summary.scalar('l2_inf', l2_inf)
+            tf.summary.scalar('l2_itp', l2_itp)
+
+        train_step = self.train_step(grads, self.optimizers['train'], summary_verbose=self.p.train_verbose)
+        self.train_steps['train'] = train_step
+        self.summary_ops['all'] = tf.summary.merge_all()
+
+        self.feed_dict['train'] = ['data', 'label', 'training']
+        self.feed_dict['predict'] = ['data', 'training']
+        self.feed_dict['summary'] = ['data', 'label', 'training']
 
         self.run_op['train'] = {'train_step': self.train_steps['train'],
                                 'loss': self.losses['train'], 'global_step': self.gs}
